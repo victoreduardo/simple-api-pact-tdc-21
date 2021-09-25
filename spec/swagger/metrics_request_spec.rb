d7004e67a6be5118472a5fe57c56980cf0cd714a
Victor Eduardo de Sousa Silva <veduardosousa@apple.com>
2021-09-24 21:04:01 -0300

    rubocop

--- spec/swagger/metrics_request_spec.rb
+++ spec/swagger/metrics_request_spec.rb
@@ -27,18 +27,20 @@ RSpec.describe Metric, type: :request do
       consumes 'application/json'
       produces 'application/json'
 
-      parameter name: :metric, in: :body, schema: {
-        type: :object,
-        properties: {
-          name: { type: :string },
-          value: { type: :number },
-          date: { type: 'date-time' }
-        },
-        required: ['name', 'value', 'date']
-      }
+      parameter name: :metric,
+        in: :body,
+        schema: {
+          type: :object,
+          properties: {
+            name: { type: :string },
+            value: { type: :number },
+            date: { type: 'date-time' }
+          },
+          required: %w[name value date]
+        }
 
       response '200', 'OK' do
-        let!(:initial_count) { Metric.count }
+        let!(:initial_count) { described_class.count }
 
         schema type: :object,
           properties: {
@@ -46,12 +48,12 @@ RSpec.describe Metric, type: :request do
             name: { type: :string, nullable: false },
             value: { type: :number, nullable: false },
             date: { type: 'date-time', nullable: false },
-            created_at: { type: :datetime, nullable: false },
-            updated_at: { type: :date, nullable: false }
+            created_at: { type: 'date-time', nullable: false },
+            updated_at: { type: 'date-time', nullable: false }
           }
 
         run_test! do
-          expect(Metric.count).to eq(initial_count + 1)
+          expect(described_class.count).to eq(initial_count + 1)
         end
       end
     end
